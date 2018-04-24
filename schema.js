15cc5a594a1e2358a18ff357225569351f4f1c86
willardshikami <willardshikami@gmail.com>
2018-04-24 10:17:02 +0300

    mongoose & tooling setup

---
+++ schema.js
@@ -0,0 +1,15 @@
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
+} = require('graphql');
+
+
+//Root Query
+
+module.exports = new GraphQLSchema({
+
+});
--- server.js
+++ server.js
@@ -1,13 +1,33 @@
-const express = require('express');
-const expressGraphQL = require('express-graphql');
+import express from 'express';
+import bodyParser from 'body-parser';
+import graphqlHTTP from 'express-graphql';
+import mongoose from 'mongoose';
+
+//import schema from './graphql';
 
 const app = express();
 
-app.use('/graphql', expressGraphQL({
-  schena: schema,
-  graphiql: true
-}));
+//Mongoose Setup
+mongoose.connect('mongodb://willies:wssnu9295@ds251179.mlab.com:51179/roamkenya');
+const db = mongoose.connection;
 
-app.listen(4000, () => {
-  console.log("Server is running on port 4000...")
-})
+//Checking for DB connection 
+db.on('error', () => console.log('Failed to connect to Database'));
+db.once('open', () => console.log('Connected to Database'));
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+});
+
+//GraphQL API Endpoint
+app.use('./graphql', graphqlHTTP(() => ({
+  schema,
+  graphiql: true,
+  pretty: true
+})));
+
+//Starting the Server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
