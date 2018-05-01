cb8c7993d710935c42510be0c471ddcfc375f41d
willardshikami <willardshikami@gmail.com>
2018-05-01 07:30:04 +0300

    added react client side

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div id="name">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
