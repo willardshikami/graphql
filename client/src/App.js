2a00889bd38cc2f84b21ddd9635b2bd05cc7dc4f
willardshikami <willardshikami@gmail.com>
2018-05-03 12:24:01 +0300

    new component for the book list

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+//components
+import BookList from './components/BookList'
 
 class App extends Component {
   render() {
     return (
       <div id="name">
-        
+        <BookList/>
       </div>
     );
   }
---
+++ client/src/components/BookList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class BookList extends Component {
+  render() {
+    return (
+      <div>
+        <ul id="book-list">
+          <li>Book List</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
