ae7303afc18d3cedaf3889ba32c5e524977ffb83
willardshikami <willardshikami@gmail.com>
2018-05-07 05:38:03 +0300

    apollo setup + redering data to component

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
 
+const getBooksQuery = gql`
+  {
+    books{
+      name
+      id
+    }
+  }
+`
 
 class BookList extends Component {
+  displayBooks(){
+    var data = this.props.data;
+    if(data.loading){
+      return(<div>Loading Books...</div>)
+    }else{
+      return data.books.map(book => {
+        return(<li key={book.id}>{book.name}</li>)
+      })
+    }
+  }
   render() {
     return (
       <div>
         <ul id="book-list">
-          <li>Book List</li>
+          { this.displayBooks() }
         </ul>
       </div>
     );
   }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+//allow cross origin requests
+app.use(cors());
 
 //connect to database
 mongoose.connect('mongodb://shikami:shikami@ds263109.mlab.com:63109/gqlshikami');
